e8f054bb5d4606442e60226d2c1271de735e5969
unknown <ajibadeifedolapo17@gmail.com>
2024-06-17 17:05:44 +0100

    final pushes

--- src/app/thank-you/page.js
+++ src/app/thank-you/page.js
@@ -2,6 +2,7 @@
 
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
+import Image from 'next/image';
 
 const ThankYou = () => {
   const router = useRouter();
@@ -15,13 +16,23 @@ const ThankYou = () => {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center mx-auto">
-        <div className=''>
-      <h1>Thank You!</h1>
+        <div className='mx-6 lg:mx-0 p-8 bg-dark-blue text-white rounded-3xl gap-5 flex flex-col max-w-[400px] justify-center items-center'>
+            {/* the div for the image up top */}
+      <div className="p-2 flex justify-center">
+      <Image src="/illustration-thank-you.svg" width={150} height={150} />
+      </div>
+        {/* for the rating score */}
+        <div className='rounded-full bg-gray-700 p-2 max-w-fit'>
       {rating ? (
-        <p>Congratulations, you selected the number {rating} out of 5.</p>
+        <p className='text-orange text-sm md:font-bold font-normal text-center'>You selected {rating} out of 5.</p>
       ) : (
-        <p>Loading...</p>
+        <p className='text-orange text-sm md:font-bold font-normal text-center'>Loading...</p>
       )}
+      </div>
+      <h1 className='text-white font-semibold text-2xl text-center'>Thank You!</h1>
+      <div>
+        <p className='text-gray-500 font-medium text-center text-sm'>We appreciate you taking the time to give a rating, if you ever need more supprt, don't hesitate to get in touch!</p>
+      </div>
     </div>
     </main>
     
